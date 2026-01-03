1cb7d51a6ccc2503413f8bd431379035d220b170
Luke Young <github@bored.engineer>
2026-01-02 21:37:31 -0800

    feat: add pebble backend

---
+++ pebble/storage.go
@@ -0,0 +1,75 @@
+package pebblestorage
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/cockroachdb/pebble"
+)
+
+// Implements the ghtransport.Storage interface using github.com/cockroachdb/pebble.
+type Storage struct {
+	DB           *pebble.DB
+	WriteOptions *pebble.WriteOptions
+}
+
+func (s *Storage) Get(ctx context.Context, u *url.URL) (_ *http.Response, rerr error) {
+	key := []byte(u.String())
+	value, closer, err := s.DB.Get(key)
+	if err == pebble.ErrNotFound {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("(*pebble.DB).Get failed: %w", err)
+	}
+	defer func() {
+		if err := closer.Close(); err != nil && rerr == nil {
+			rerr = fmt.Errorf("(*io.ReadCloser).Close failed: %w", err)
+		}
+	}()
+
+	// Make a copy of the value since it's only valid until closer.Close()
+	bodyBytes := make([]byte, len(value))
+	copy(bodyBytes, value)
+
+	resp, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(bodyBytes)), nil)
+	if err != nil {
+		return nil, fmt.Errorf("http.ReadResponse failed: %w", err)
+	}
+	return resp, nil
+}
+
+func (s *Storage) Put(ctx context.Context, u *url.URL, resp *http.Response) error {
+	b, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		return fmt.Errorf("httputil.DumpResponse failed: %w", err)
+	}
+	key := []byte(u.String())
+	if err := s.DB.Set(key, b, s.WriteOptions); err != nil {
+		return fmt.Errorf("(*pebble.DB).Set failed: %w", err)
+	}
+	return nil
+}
+
+// Open is a wrapper around pebble.Open that returns an initialized Storage.
+func Open(path string, opts *pebble.Options) (*Storage, error) {
+	db, err := pebble.Open(path, opts)
+	if err != nil {
+		return nil, fmt.Errorf("pebble.Open failed: %w", err)
+	}
+	return &Storage{DB: db, WriteOptions: pebble.Sync}, nil
+}
+
+// MustOpen is a wrapper around Open that panics if an error occurs.
+func MustOpen(path string, opts *pebble.Options) *Storage {
+	s, err := Open(path, opts)
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
